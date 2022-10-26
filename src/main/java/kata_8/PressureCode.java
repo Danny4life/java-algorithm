83d14b777be4e059af402173e44e1657795b47c3
Segun Osiki <osiki.segun@gmail.com>
2022-10-26 07:06:03 +0100

    adding files

---
+++ src/main/java/kata_8/PressureCode.java
@@ -0,0 +1,18 @@
+package kata_8;
+
+
+//Code as fast as you can! You need to double the integer and return it.
+
+
+
+public class PressureCode {
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(3));
+    }
+
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i*2;
+    }
+}
