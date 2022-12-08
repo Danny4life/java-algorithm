7d00030861147c2a710021c11c18d02438366b59
Segun Osiki <osiki.segun@gmail.com>
2022-12-08 08:47:42 +0100

    adding files

---
+++ src/main/java/kata_8/BMI.java
@@ -0,0 +1,33 @@
+package kata_8;
+
+//Write function bmi that calculates body mass index (bmi = weight / height2).
+//
+//        if bmi <= 18.5 return "Underweight"
+//
+//        if bmi <= 25.0 return "Normal"
+//
+//        if bmi <= 30.0 return "Overweight"
+//
+//        if bmi > 30 return "Obese"
+
+public class BMI {
+
+    public static void main(String[] args) {
+        System.out.println(bmi(80, 1.80));  //normal
+    }
+
+    public static String bmi(double weight, double height) {
+
+
+        double res = weight / (height * height);
+
+        if (res <= 18.5){
+            return "Underweight";
+        } else if (res <= 25.0) {
+            return "Normal";
+        } else if (res <= 30.0) {
+            return "Overweight";
+        }
+            return "Obese";
+    }
+}
