7b094679ec355ecc791b11649e9cc2d56b839848
Segun Osiki <osiki.segun@gmail.com>
2022-12-12 08:06:43 +0100

    adding files

---
+++ src/main/java/kata_8/BasicVariableAssignment.java
@@ -0,0 +1,17 @@
+package kata_8;
+
+
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+
+public class BasicVariableAssignment {
+
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+}
