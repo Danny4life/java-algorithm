da43a74c264834443bc033d53265f27ac6315d09
Segun Osiki <osiki.segun@gmail.com>
2023-01-01 04:52:34 +0100

    added Array of Array class to kata 8

---
+++ src/main/java/kata_8/ArrayOfArray.java
@@ -0,0 +1,28 @@
+package kata_8;
+
+//I'm new to coding and now I want to get the sum of two arrays...
+//        Actually the sum of all their elements. I'll appreciate for your help.
+//
+//        P.S. Each array includes only integer numbers. Output is a number too.
+
+//public class SolutionTest {
+//    @Test
+//    public void sampleTests() {
+//        assertEquals(21, Sum.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+//        assertEquals(-21, Sum.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
+//        assertEquals(15, Sum.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
+//        assertEquals(2100, Sum.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
+//    }
+//}
+
+public class ArrayOfArray {
+    public static void main(String[] args) {
+        System.out.println(arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+    }
+
+    public static int arrayPlusArray(int[] arr1, int[] arr2) {
+        // arr1 + arr2 is not working...
+        return 0;
+    }
+
+}
