0f92f2ed4a82f917ff75c262acc244f2013c23f3
Segun Osiki <osiki.segun@gmail.com>
2023-01-01 05:08:06 +0100

    solved Array of Array algorithm

--- src/main/java/kata_8/ArrayOfArray.java
+++ src/main/java/kata_8/ArrayOfArray.java
@@ -15,14 +15,28 @@ package kata_8;
 //    }
 //}
 
+//Steps
+//-- sum all array 1
+// sum all array 2
+//-- add array 1 and array 2 together
+
 public class ArrayOfArray {
     public static void main(String[] args) {
-        System.out.println(arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+        System.out.println(arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
     }
 
     public static int arrayPlusArray(int[] arr1, int[] arr2) {
         // arr1 + arr2 is not working...
-        return 0;
+
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 0; i < arr1.length; i++){
+            sum1 += arr1[i];
+        }
+        for(int j = 0; j < arr2.length; j++){
+            sum2 += arr2[j];
+        }
+        return (sum1 + sum2);
     }
 
 }
