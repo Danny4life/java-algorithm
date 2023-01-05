6a2199aa08b9cde5fc14b4559183970369536dfa
Segun Osiki <osiki.segun@gmail.com>
2023-01-05 07:32:37 +0100

    add SortAndStar class to kata 8

---
+++ src/main/java/kata_8/SortAndStar.java
@@ -0,0 +1,32 @@
+package kata_8;
+
+//You will be given a list of strings. You must sort it alphabetically
+//        (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+//
+//        The returned value must be a string, and have "***" between each of its letters.
+//
+//        You should not remove or add elements from/to the array.
+//
+
+
+//public class SolutionTest {
+//
+//    @Test
+//    public void testFixed() {
+//        assertEquals("b***i***t***c***o***i***n", SortAndStar.twoSort(new String[]
+//                {"bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"}));
+//        assertEquals("a***r***e", SortAndStar.twoSort(new String[]
+//                {"turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"}));
+//    }
+//}
+
+public class SortAndStar {
+
+    public static void main(String[] args) {
+        System.out.println(twoSort());
+    }
+
+    public static String twoSort(String[] s) {
+        return null;
+    }
+}
