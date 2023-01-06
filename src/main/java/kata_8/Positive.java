c687e0c50f12ca33552baf5d2ff367ae1132602d
Segun Osiki <osiki.segun@gmail.com>
2023-01-06 11:18:30 +0100

    add Positive class algotithm

---
+++ src/main/java/kata_8/Positive.java
@@ -0,0 +1,29 @@
+package kata_8;
+
+//You get an array of numbers, return the sum of all of the positives ones.
+//
+//        Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//
+//        Note: if there is nothing to sum, the sum is default to 0.
+//
+
+//public class SolutionTest {
+//    @Test
+//    public void testSomething() {
+//        assertEquals(15, Positive.sum(new int[]{1,2,3,4,5}));
+//        assertEquals(13, Positive.sum(new int[]{1,-2,3,4,5}));
+//        assertEquals(0, Positive.sum(new int[]{}));
+//        assertEquals(0, Positive.sum(new int[]{-1,-2,-3,-4,-5}));
+//        assertEquals(9, Positive.sum(new int[]{-1,2,3,4,-5}));
+//    }
+//}
+public class Positive {
+
+    public static void main(String[] args) {
+        System.out.println(sum(new int[]{1,2,3,4,5}));
+    }
+
+    public static int sum(int[] arr){
+        return 0;
+    }
+}
