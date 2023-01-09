55417e69d07d25c6e61d7f909139bd5e780ce2fc
Segun Osiki <osiki.segun@gmail.com>
2023-01-09 11:20:53 +0100

    add Cosonant class algorithm

---
+++ src/main/java/kata_7/Cosonant.java
@@ -0,0 +1,35 @@
+package kata_7;
+
+//Write a function consonantCount, consonant_count or ConsonantCount that takes a
+//        string of English-language text and returns the number of consonants in the string.
+//
+//        Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+
+//public class ConsonantsTest {
+//
+//    @Test
+//    public void testCase1() {
+//        assertEquals("Test string is empty string",0,Consonants.getCount(""));
+//        assertEquals("Test string: \"aaaaa\"",0,Consonants.getCount("aaaaa"));
+//        assertEquals("Test string: \"XaeiouX\"",2,Consonants.getCount("XaeiouX"));
+//        assertEquals("Test string: \"Bbbbb\"",5,Consonants.getCount("Bbbbb"));
+//        assertEquals("Test string: \"helLo world\"",7,Consonants.getCount("helLo world"));
+//        assertEquals("Test string: \"h^$&^#$&^elLo world\"",7,Consonants.getCount("h^$&^#$&^elLo world"));
+//        assertEquals("Test string: \"012345_Cb\"",2,Consonants.getCount("012345_Cb"));
+//        assertEquals("Test string: \"0123456789\"",0,Consonants.getCount("0123456789"));
+//    }
+//
+//}
+
+public class Cosonant {
+
+    public static void main(String[] args) {
+        System.out.println(getCount("aaaaa"));
+    }
+
+    public static int getCount(String str) {
+        // ...
+
+        return 0;
+    }
+}
