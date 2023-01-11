3196324d7878c302ca5e83da1eac1bba93a7a209
Segun Osiki <osiki.segun@gmail.com>
2023-01-11 09:45:23 +0100

    add Katawells class

---
+++ src/main/java/kata_8/KataWells.java
@@ -0,0 +1,29 @@
+package kata_8;
+
+//For every good kata idea there seem to be quite a few bad ones!
+//
+//        In this kata you need to check the provided array (x) for good ideas 'good'
+//        and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!',
+//        if there are more than 2 return 'I smell a series!'. If there are no good ideas,
+//        as is often the case, return 'Fail!'.
+
+//public class SolutionTest {
+//    @Test
+//    public void basicTests() {
+//        assertEquals("Fail!", Kata.well(new String[] {"bad", "bad", "bad"}));
+//        assertEquals("Publish!", Kata.well(new String[] {"good", "bad", "bad", "bad", "bad"}));
+//        assertEquals("I smell a series!", Kata.well(new String[] {
+//                "good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"}));
+//    }
+//}
+public class KataWells {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static String well(String[] x) {
+        // TODO
+    }
+
+}
