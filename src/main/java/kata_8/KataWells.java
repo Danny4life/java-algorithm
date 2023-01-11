cfcb44d03865a99177eceb42312767b06dccd91c
Segun Osiki <osiki.segun@gmail.com>
2023-01-11 10:54:49 +0100

    solved Kata wells algorithm

--- src/main/java/kata_8/KataWells.java
+++ src/main/java/kata_8/KataWells.java
@@ -16,14 +16,31 @@ package kata_8;
 //                "good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"}));
 //    }
 //}
+
+//STEPS
+// check the number of good present
+// if it is one or two return publish
+// if it is more than two return i smell a series
+// if there is no good idea return fail
 public class KataWells {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(well(new String[]{"good", "good", "good", "bad", "bad", "bad"}));
     }
 
     public static String well(String[] x) {
         // TODO
+
+        int counter = 0;
+
+        for(String s : x){
+            if(s == "good"){
+                counter += 1;
+            }
+        }
+
+        return counter == 0 ? "Fail!" : counter > 2 ? "I smell a series!" : "Publish!";
+
     }
 
 }
