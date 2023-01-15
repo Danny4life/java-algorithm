94fa2f1fccf5104cc746f200b3e0a89f696d6fd3
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 21:58:10 +0100

    add Hello algorithm class

---
+++ src/main/java/kata_8/Hello.java
@@ -0,0 +1,33 @@
+package kata_8;
+//Create a method sayHello/say_hello/SayHello that takes as input a name,
+//        city, and state to welcome a person. Note that name will be an
+//        array consisting of one or more values that should be joined together
+//        with one space between each, and the length of the name array in test cases will vary.
+//
+//        Example:
+//
+//        sayHello(new String[]{"John", "Smith"}, "Phoenix", "Arizona")
+//        This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+
+//public class HelloTest {
+//
+//    @Test
+//    public void test1() throws Exception {
+//        Hello h = new Hello();
+//        String[] name = {"John", "Smith"};
+//        assertEquals("Hello, John Smith! Welcome to Phoenix, Arizona!",
+//                h.sayHello(name, "Phoenix", "Arizona"));
+//    }
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello(String [] name, String city, String state){
+        //code here
+    }
+}
