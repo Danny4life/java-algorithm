5060200a3f8a38f52f02d9c4202f180e67358e96
Segun Osiki <osiki.segun@gmail.com>
2023-01-19 12:43:24 +0100

    add Angle sum class algorithm

---
+++ src/main/java/kata_7/AngleSums.java
@@ -0,0 +1,25 @@
+package kata_7;
+//
+//Find the total sum of internal angles (in degrees)
+//        in an n-sided simple polygon. N will be greater than 2.
+
+
+//public class SolutionTest {
+//    @Test
+//    public void sampleTests() {
+//        assertEquals(180, AngleSum.sumOfAngles(3));
+//        assertEquals(360, AngleSum.sumOfAngles(4));
+//    }
+//}
+public class AngleSums {
+
+    public static void main(String[] args) {
+        System.out.println(sumOfAngles(3));
+    }
+
+    public static int sumOfAngles(int n) {
+        // Code here
+
+        return 0;
+    }
+}
