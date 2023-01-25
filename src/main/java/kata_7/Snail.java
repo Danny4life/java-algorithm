2e959d43961cd83a14c3ef36da4b0f65980db853
Segun Osiki <osiki.segun@gmail.com>
2023-01-25 14:00:08 +0100

    add snail algorithm

---
+++ src/main/java/kata_7/Snail.java
@@ -0,0 +1,36 @@
+package kata_7;
+
+//The snail crawls up the column. During the day it crawls up some distance.
+//        During the night she sleeps, so she slides down for some distance
+//        (less than crawls up during the day).
+//
+//        Your function takes three arguments:
+//
+//        The height of the column (meters)
+//        The distance that the snail crawls during the day (meters)
+//        The distance that the snail slides down during the night (meters)
+//        Calculate number of day when the snail will reach the top of the column.
+
+
+//ublic class SampleTest {
+//    @Test
+//    public void basicTests() {
+//        assertEquals(2, Kata.snail(3,2,1));
+//        assertEquals(5, Kata.snail(10,3,1));
+//        assertEquals(8, Kata.snail(10,3,2));
+//        assertEquals(7, Kata.snail(100,20,5));
+//        assertEquals(1, Kata.snail(5,10,3));
+//    }
+//}
+
+
+public class Snail {
+
+    public static void main(String[] args) {
+        System.out.println(snail(3,2,1));
+    }
+
+    public static int snail(int column, int day, int night) {
+        return 0; // your code here
+    }
+}
