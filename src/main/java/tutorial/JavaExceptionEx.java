0e93e9d2e8fc9bff7eec3be523406b6d630ff7c0
Danny4life <osiki.segun@gmail.com>
2023-10-19 08:44:11 +0100

    implement exception class

--- src/main/java/tutorial/JavaExceptionEx.java
+++ src/main/java/tutorial/JavaExceptionEx.java
@@ -1,13 +1,24 @@
 package tutorial;
 
+import java.util.Arrays;
+
 public class JavaExceptionEx {
     public static void main(String[] args) {
+
+
+        int[] a = new int[5];
+        a[4] = 50;
+
+        System.out.println(Arrays.toString(a));
+
         try{
-            int data = 100/0;
-        } catch (ArithmeticException e) {
+            String s = null;
+            System.out.println(s.length());
+        } catch (NullPointerException e) {
             System.out.println(e);
+
         }
-        System.out.println("rest of the code...");
+        System.out.println("rest of the code");
 
 
     }
