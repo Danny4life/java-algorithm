fbad751f55353b874abd3c658317421a9818e10f
Danny4life <osiki.segun@gmail.com>
2023-10-20 07:05:37 +0100

    create a multiple try catch exception class

---
+++ src/main/java/tutorial/MultipleTryCatchExceptionEx.java
@@ -0,0 +1,21 @@
+package tutorial;
+
+public class MultipleTryCatchExceptionEx {
+
+    public static void main(String[] args) {
+        try{
+            String s = null;
+            System.out.println(s.length());
+        }catch (ArithmeticException e){
+            System.out.println("Arithmetic exception occurs");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Arrayoutofbound exception occurs");
+        }
+        catch (Exception e){
+            System.out.println("Parent exception occurs");
+        }
+
+        System.out.println("Rest of the code");
+    }
+}
