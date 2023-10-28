183df2ba1ff498f2f133090f6f96d8070d00a12f
Danny4life <osiki.segun@gmail.com>
2023-10-28 07:08:51 +0100

    create input stream class example

---
+++ src/main/java/javaio/InputStreamEx1.java
@@ -0,0 +1,28 @@
+package javaio;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class InputStreamEx1 {
+    public static void main(String[] args) {
+
+        // Get the user's home directory
+        String userHome = System.getProperty("user.home");
+
+        // Construct the path to the desktop directory
+        String desktopPath = userHome + "/Desktop/testing.txt";
+
+        try{
+            FileInputStream in = new FileInputStream(desktopPath);
+           int i = in.read();
+            System.out.println((char)i);
+
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
