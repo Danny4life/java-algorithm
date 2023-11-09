7db9f880988a4455a18d5c78f883d81154837464
Danny4life <osiki.segun@gmail.com>
2023-11-09 10:51:36 +0100

    implement local date

--- src/main/java/tutorial/LocalDateEx.java
+++ src/main/java/tutorial/LocalDateEx.java
@@ -1,16 +1,19 @@
 package tutorial;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class LocalDateEx {
     public static void main(String[] args) {
         LocalDate date = LocalDate.now();
         LocalDate yesterday = date.minusDays(1);
         LocalDate tomorrow = date.plusDays(2);
+        LocalTime time = LocalTime.now();
 
         System.out.println("today date " + date);
         System.out.println("yesterday date " + yesterday);
         System.out.println("tomorrow data " + tomorrow);
+        System.out.println("time is " + time);
 
     }
 }
