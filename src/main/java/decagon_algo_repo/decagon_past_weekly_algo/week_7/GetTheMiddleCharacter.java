404c41e4cb44126ebbb23e5cd1f9a463aaceaf79
Danny4life <osiki.segun@gmail.com>
2024-03-05 08:48:15 +0100

    commit files

---
+++ src/main/java/decagon_algo_repo/decagon_past_weekly_algo/week_7/GetTheMiddleCharacter.java
@@ -0,0 +1,61 @@
+package decagon_algo_repo.decagon_past_weekly_algo.week_7;
+
+
+//You are going to be given a word. Your job is to return the middle character of the word.
+//        If the word's length is odd, return the middle character. If the word's length is even,
+//        return the middle 2 characters.
+//
+//        #Examples:
+//
+//        Kata.getMiddle("test") should return "es"
+//
+//        Kata.getMiddle("testing") should return "t"
+//
+//        Kata.getMiddle("middle") should return "dd"
+//
+//        Kata.getMiddle("A") should return "A"
+//        #Input
+//
+//        A word (string) of length 0 < str < 1000 (In javascript you may get slightly
+//        more than 1000 in some test cases due to an error in the test cases).
+//        You do not need to test for this. This is only here to tell you
+//        that you do not need to worry about your solution timing out.
+//
+//        #Output
+//
+//        The middle character(s) of the word represented as a string.
+//
+//
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//
+//public class MiddleExampleTests {
+//    @Test
+//    public void evenTests() {
+//        assertEquals("es", Kata.getMiddle("test"));
+//        assertEquals("dd", Kata.getMiddle("middle"));
+//    }
+//
+//    @Test
+//    public void oddTests() {
+//        assertEquals("t", Kata.getMiddle("testing"));
+//        assertEquals("A", Kata.getMiddle("A"));
+//    }
+//}
+
+
+
+public class GetTheMiddleCharacter {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String getMiddle(String word) {
+        //Code goes here!
+
+        return "";
+    }
+}
+
+// https://www.codewars.com/kata/56747fd5cb988479af000028/train/java
---
+++ src/main/java/decagon_algo_repo/decagon_past_weekly_algo/week_7/NumberToOrdinal.java
@@ -0,0 +1,73 @@
+package decagon_algo_repo.decagon_past_weekly_algo.week_7;
+
+//Task
+//        Finish the function numberToOrdinal, which should take a number and
+//        return it as a string with the correct ordinal indicator suffix (in English).
+//        For example, 1 turns into "1st".
+//
+//        For the purposes of this challenge, you may assume that the function will always
+//        be passed a non-negative integer. If the function is given 0 as an argument,
+//        it should return the string "0" without a suffix.
+//
+//        To help you get started, here is an excerpt from Wikipedia's page on Ordinal Indicators:
+//
+//        st is used with numbers ending in 1 (e.g. 1st, pronounced first)
+//        nd is used with numbers ending in 2 (e.g. 92nd, pronounced ninety-second)
+//        rd is used with numbers ending in 3 (e.g. 33rd, pronounced thirty-third)
+//        As an exception to the above rules, all the "teen"
+//        numbers ending with 11, 12 or 13 use -th
+//        (e.g. 11th, pronounced eleventh, 112th, pronounced one hundred [and] twelfth)
+//        th is used for all other numbers (e.g. 9th, pronounced ninth).
+//        Specification
+//        Challenge.numberToOrdinal(number)
+//        take a number and return it as a string with the correct English ordinal indicator suffix
+//
+//        Parameters
+//        number: Integer - The number to be converted to a string ordinal
+//
+//        Return Value
+//        String - Returns a string ordinal based on the number.
+//
+//        Constraints
+//        0 ≤ number ≤ 10000
+//
+//        Examples
+//        number	Return Value
+//        1	"1st"
+//        2	"2nd"
+//        3	"3rd"
+//        4	"4th"
+//        21	"21st"
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Nested;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@DisplayName("numberToOrdinal")
+//public class NumberToOrdinalTest {
+//    @Nested
+//    @DisplayName("sample tests")
+//    class SampleTests {
+//
+//        @Test
+//        @DisplayName("should handle single digits")
+//        public void shouldHandleSingleDigits() {
+//            assertEquals("1st", Challenge.numberToOrdinal(1));
+//            assertEquals("2nd", Challenge.numberToOrdinal(2));
+//            assertEquals("3rd", Challenge.numberToOrdinal(3));
+//        }
+//    }
+//}
+
+
+public class NumberToOrdinal {
+    public static void main(String[] args) {
+
+    }
+
+    public static String numberToOrdinal(Integer number) {
+
+        return "";
+    }
+}
